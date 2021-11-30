8ba5bf60559d70af7f11628b26ea9c5f08d703eb
sideswiper22 <58717491+sideswiper22@users.noreply.github.com>
2021-11-30 14:06:42 +0800

    added loader

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+
+
 var nav = 0;
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+window.addEventListener('load', OnPageLoad());
 
-tl.fromTo(".landing", {y:-50,opacity: 0}, {y:0,opacity:1,duration: 0.5});
+//function that disables the loading screen once the page loads completely
+function OnPageLoad() {
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
 
-function ToggleNav(){
+    tl.fromTo(".landing", {y:-50,opacity: 0}, {y:0,opacity:1,duration: 0.5, delay: 0.5});
+}
+
+function ToggleNav() {
     if (nav==0) {
-        tl.fromTo(".nav-menu", {x:-200}, {x:0,duration: 0.25});
+        tl.fromTo('.sidebar', {x:-256}, {x:0,duration: 0.25});
         nav = 1;
     }
     else {
-        tl.fromTo(".nav-menu", {x:0}, {x:-200, duration: 0.25});
+        tl.fromTo('.sidebar', {x:0}, {x:-256, duration: 0.25});
         nav = 0;
     }
 
